c4256878d1a05e4c3d933149c491c587333de881
chaojun <zhchj0910@126.com>
2017-06-09 17:56:59 +0800

    branch1  loge  how to use branch?

--- app/src/main/java/com/hellogithub/MainActivity.java
+++ app/src/main/java/com/hellogithub/MainActivity.java
@@ -2,12 +2,16 @@ package com.hellogithub;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "cj";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.e(TAG, "onCreate: 新分支1" );
+
     }
 }
